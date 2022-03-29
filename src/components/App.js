8d527d1e623ac90046a2abb55f45f101d40a14c2
jisshub <jissmon476.com>
2022-03-29 16:48:33 +0530

    build list with redux data

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,12 @@ import SongList from './SongList';
 
 function App() {
   return (
-    <div className="App">
-      <SongList />
+    <div className="ui container grip">
+      <div className='ui row'>
+        <div className='column eight wide'>
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 }
